c744fb2794b3bcec74179fab6c40643a62dac332
tarcisio <tarcisio.almeida@totvs.com.br>
2020-04-01 15:36:51 -0300

    corrigi um ; e apresentei uma linha no println

--- TestaCaracteres.java
+++ TestaCaracteres.java
@@ -12,12 +12,13 @@ public class TestaCaracteres {
 		System.out.println(valor);
 		
 		String palavra = "alura cursos online de tecnologia";
-		String palavra1 = "testanto o Git"
+		String palavra1 = "testanto o Git";
 		System.out.println(palavra);
 		
 		palavra = palavra + 2020;
 		
 		System.out.println(palavra);
+		System.out.println(palavra1);
 
 	}
 }
